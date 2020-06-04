51f27af49fa0a886a43f28d05a09f713bea8212a
JaronArmiger <jaron.armiger@gmail.com>
2020-06-04 10:51:59 -0500

    configure action_mailer with heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "airbnb_clone_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://glacial-oasis-76060.herokuapp.com/'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
