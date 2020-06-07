f3cd251fb64122f2f6f33c8af1c947cd6f78105a
JaronArmiger <jaron.armiger@gmail.com>
2020-06-07 13:06:00 -0500

    implement map on each location page

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,7 +1,3 @@
 module MapsHelper
-	def map_url
-		google_maps_api_key = ENV["GOOGLE_MAPS_API_KEY"]
-		root_url = "https://www.google.com/maps/embed/v1/place?"
-		address = Location.format_address
-	end
+	
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,31 @@
 class Location < ApplicationRecord
+	def format_address
+		url_string = ""
+		if street_number
+			url_string << street_number + "+"
+		end
+		if street
+			url_string << street + "+"
+		end
+		if city
+			url_string << city + "+"
+		end
+		if state
+			url_string << state + "+"
+		end
+		if country_code
+			url_string << country_code.upcase + "+"
+		end
+		if postal_code
+			url_string << postal_code + "+"
+		end
+		url_string = url_string.chop
+	end
+
+	def map_url
+		key = ENV["GOOGLE_MAPS_API_KEY"]
+		root_url = "https://www.google.com/maps/embed/v1/place?"
+		address = format_address
+		url = root_url + "q=" + address + "&key=" + key
+	end
 end
