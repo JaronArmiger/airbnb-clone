8a98b3fe8f9913b49c58a2724f98a3d210e424b4
JaronArmiger <jaron.armiger@gmail.com>
2020-06-07 13:56:51 -0500

    generate model for Space, Type, and Amenity

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_211034) do
+ActiveRecord::Schema.define(version: 2020_06_07_185552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "amenities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "country_code"
@@ -25,6 +31,18 @@ ActiveRecord::Schema.define(version: 2020_06_06_211034) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "state"
     t.string "country"
+  end
+
+  create_table "spaces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
