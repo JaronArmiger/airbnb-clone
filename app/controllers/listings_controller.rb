a56941f6e92d642fd55bbab03b904485ecc6c19a
JaronArmiger <jaron.armiger@gmail.com>
2020-06-10 11:29:28 -0500

    implement multi-page form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -9,16 +9,45 @@ class ListingsController < ApplicationController
 	end
 
 	def new
-		@listing = Listing.new
-		@listing.build_location
+		session[:listing_params] ||= {}
+		@listing = Listing.new(session[:listing_params])
+		@listing.current_step = session[:listing_step]
 	end
 
 	def create
-		@listing = Listing.new(listing_params)
-		if @listing.save
-			redirect_to listing_path(@listing)
-		else
+		session[:listing_params].deep_merge!(listing_params) if params[:listing]
+		@listing = Listing.new(session[:listing_params])
+		
+		@listing.current_step = session[:listing_step]
+
+		puts "@listing"
+		p @listing
+=begin
+		puts ""
+		puts "session[:listing_params]"
+		p session[:listing_params]
+		puts "@listing.current_step"
+		p @listing.current_step
+		puts "session[:listing_step]"
+		p session[:listing_step]
+		puts ""
+=end	
+		if @listing.valid?
+			if params[:back_button]
+				@listing.previous_step
+			elsif @listing.last_step?
+				@listing.save if @listing.all_valid?
+			else
+				@listing.next_step
+			end
+			session[:listing_step] = @listing.current_step
+		end
+		if @listing.new_record? # returns true if object has not been saved yet
 			render :new
+		else
+			session[:listing_step] = session[:listing_params] = nil
+			flash[:notice] = "Listing Saved"
+			redirect_to listing_path(@listing)
 		end
 	end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,12 +8,12 @@ class Listing < ApplicationRecord
 	belongs_to :type
 
 	# validations
-	validates :type_id, presence: true
-	validates :guests, presence: true, numericality: { only_integer: true }
-	validates :beds, presence: true, numericality: { only_integer: true }
-	validates :bathrooms, presence: true, numericality: { only_integer: true }
-	validates :description, presence: true, length: { maximum: 500 }
-	validates :title, presence: true, length: { maximum: 30 }
+	validates :type_id, presence: true, if: lambda { |l| l.current_step == "type"}
+	validates :guests, presence: true, numericality: { only_integer: true }, if: lambda { |l| l.current_step == "guests_beds"}
+	validates :beds, presence: true, numericality: { only_integer: true }, if: lambda { |l| l.current_step == "guests_beds"}
+	validates :bathrooms, presence: true, numericality: { only_integer: true }, if: lambda { |l| l.current_step == "bathrooms"}
+	validates :description, presence: true, length: { maximum: 500 }, if: lambda { |l| l.current_step == "description"}
+	validates :title, presence: true, length: { maximum: 30 }, if: lambda { |l| l.current_step == "title" }
 
 	attr_writer :current_step
 
@@ -24,6 +24,29 @@ class Listing < ApplicationRecord
 	end
 
 	def steps
-		%w[type guest_beds bathrooms location amenities spaces description title]
+		%w[type guests_beds bathrooms location amenities spaces description title confirmation]
+	end
+
+	def next_step
+		self.current_step = steps[steps.index(current_step) + 1]
+	end
+
+	def previous_step
+		self.current_step = steps[steps.index(current_step) - 1]
+	end
+
+	def first_step?
+		current_step == steps.first
+	end
+
+	def last_step?
+		current_step == steps.last
+	end
+
+	def all_valid?
+		steps.all? do |step|
+			self.current_step = step
+			valid?
+		end
 	end
 end
