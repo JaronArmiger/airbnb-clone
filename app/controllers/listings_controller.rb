1f78a5745eac9e7ece98c48bf71952d3ad7ea51a
JaronArmiger <jaron.armiger@gmail.com>
2020-06-10 12:57:31 -0500

    complete confirmation page of form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,6 +12,15 @@ class ListingsController < ApplicationController
 		session[:listing_params] ||= {}
 		@listing = Listing.new(session[:listing_params])
 		@listing.current_step = session[:listing_step]
+		if session[:listing_params]["location_attributes"]
+			@location = @listing.build_location(session[:listing_params]["location_attributes"])
+		end
+		if ( space_ids = session[:listing_params]["space_ids"] )
+			@spaces = Space.find(space_ids[1..-1])
+		end
+		if ( amenity_ids = session[:listing_params]["amenity_ids"] )
+			@amenities = Amenity.find(amenity_ids[1..-1])
+		end
 	end
 
 	def create
@@ -20,28 +29,60 @@ class ListingsController < ApplicationController
 		
 		@listing.current_step = session[:listing_step]
 
-		puts "@listing"
-		p @listing
-=begin
-		puts ""
-		puts "session[:listing_params]"
-		p session[:listing_params]
-		puts "@listing.current_step"
-		p @listing.current_step
-		puts "session[:listing_step]"
-		p session[:listing_step]
-		puts ""
-=end	
 		if @listing.valid?
 			if params[:back_button]
 				@listing.previous_step
+			elsif params[:review]
+				@listing.next_step
+				if session[:listing_params]["location_attributes"]
+					@location = @listing.build_location(session[:listing_params]["location_attributes"])
+				end
+				if ( space_ids = session[:listing_params]["space_ids"] )
+					@spaces = Space.find(space_ids[1..-1])
+				end
+				if ( amenity_ids = session[:listing_params]["amenity_ids"] )
+					@amenities = Amenity.find(amenity_ids[1..-1])
+				end
 			elsif @listing.last_step?
 				@listing.save if @listing.all_valid?
 			else
 				@listing.next_step
 			end
+			
 			session[:listing_step] = @listing.current_step
 		end
+
+		puts "session[:listing_params]"
+		p session[:listing_params]
+
+		puts "space_ids"
+		p space_ids
+
+		puts "session[:listing_params]['space_ids']"
+		p session[:listing_params]["space_ids"]
+		puts "@spaces"
+		p @spaces
+
+		puts "@amenities"
+		p @amenities
+=begin
+		puts "@listing"
+		p @listing
+
+		puts "@location"
+		p @location
+
+		puts ""
+		
+		#puts "session[:listing_params][:location_attributes]"
+		#p session[:listing_params]["location_attributes"]
+		puts "@listing.current_step"
+		p @listing.current_step
+		puts "session[:listing_step]"
+		p session[:listing_step]
+		puts "yes we have location_attributes"
+		puts ""
+=end
 		if @listing.new_record? # returns true if object has not been saved yet
 			render :new
 		else
@@ -55,10 +96,11 @@ class ListingsController < ApplicationController
 		def listing_params
 			params.require(:listing).permit(:type_id,
 											:guests, :beds,
-											:bathrooms, :amenity_ids,
-											:space_ids, :description,
+											:bathrooms, :description,
 											:title, location_attributes:
 											[ :country_code, :street_address,
-											 :city, :state, :postal_code ] )
+											 :city, :state, :postal_code ], 
+											 :amenity_ids => [],
+											 :space_ids => [] )
 		end
 end
