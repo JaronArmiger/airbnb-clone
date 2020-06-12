2c4bfdfd72befbf0f6507fdf57f96d970b421a11
JaronArmiger <jaron.armiger@gmail.com>
2020-06-11 19:59:50 -0500

    configure sign in with google link

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-    devise_for :users
     resources :users
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 	root 'static_pages#home'
 	get 'privacy_policy', to: 'static_pages#privacy_policy'
 	resources :locations, only: [:index, :show, :new, :create]
 	resources :listings
-
-	namespace :create_listing do
-
-	end
 
 end
