9d5f48ee184f5e25c5ebd697e7e9f109e2b7dd67
JaronArmiger <jaron.armiger@gmail.com>
2020-06-13 10:19:41 -0500

    add name field to sign up and account edit forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
 	include ApplicationHelper
 
 	before_action :store_user_location!, if: :storable_location?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	private
+
+		def configure_permitted_parameters
+			update_attrs = [:name, :email, :password, :password_confirmation, :current_password, :profile_picture]
+			devise_parameter_sanitizer.permit(:sign_up, keys: update_attrs)
+			devise_parameter_sanitizer.permit(:account_update, keys: update_attrs)
+		end
 
 		def storable_location?
 			request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  
+  protected
+    
+      def after_update_path_for(resource_or_scope)
+        user_path(resource_or_scope)
+      end
+end
